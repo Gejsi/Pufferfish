da939226c01095c2486c7254897b3fb86615d270
Gejsi <vgejsi@gmail.com>
2023-04-06 23:30:50 +0200

    Starting to work with the map

--- app/src/main/java/io/gejsi/pufferfish/controllers/MainActivity.java
+++ app/src/main/java/io/gejsi/pufferfish/controllers/MainActivity.java
@@ -1,18 +1,12 @@
-package io.gejsi.pufferfish;
+package io.gejsi.pufferfish.controllers;
 
-import android.content.ComponentName;
 import android.content.Intent;
 import android.os.Bundle;
 
-import com.google.android.material.appbar.CollapsingToolbarLayout;
 import com.google.android.material.floatingactionbutton.FloatingActionButton;
-import com.google.android.material.snackbar.Snackbar;
 
 import androidx.appcompat.app.AppCompatActivity;
 import androidx.appcompat.widget.Toolbar;
-import androidx.core.content.ContextCompat;
-
-import android.view.View;
 
 import io.gejsi.pufferfish.databinding.ActivityMainBinding;
 
