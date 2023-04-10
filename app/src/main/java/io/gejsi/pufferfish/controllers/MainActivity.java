0da2c06c5a9b73177a341ba1d6c19283ea77e6dd
Gejsi <vgejsi@gmail.com>
2023-04-10 15:29:49 +0200

    Added settings activity

--- app/src/main/java/io/gejsi/pufferfish/controllers/MainActivity.java
+++ app/src/main/java/io/gejsi/pufferfish/controllers/MainActivity.java
@@ -2,11 +2,13 @@ package io.gejsi.pufferfish.controllers;
 
 import android.content.Intent;
 import android.os.Bundle;
-
-import com.google.android.material.floatingactionbutton.FloatingActionButton;
+import android.view.View;
+import android.widget.ImageButton;
 
 import androidx.appcompat.app.AppCompatActivity;
 import androidx.appcompat.widget.Toolbar;
+
+import com.google.android.material.floatingactionbutton.FloatingActionButton;
 
 import io.gejsi.pufferfish.databinding.ActivityMainBinding;
 
@@ -27,6 +29,12 @@ public class MainActivity extends AppCompatActivity {
         FloatingActionButton fab = binding.fab;
         fab.setOnClickListener(view -> {
             Intent intent = new Intent(MainActivity.this, MapsActivity.class);
+            startActivity(intent);
+        });
+
+        ImageButton settingsButton = binding.settingsButton;
+        settingsButton.setOnClickListener((View.OnClickListener) view -> {
+            Intent intent = new Intent(MainActivity.this, SettingsActivity.class);
             startActivity(intent);
         });
     }
---
+++ app/src/main/java/io/gejsi/pufferfish/controllers/SettingsActivity.java
@@ -0,0 +1,32 @@
+package io.gejsi.pufferfish.controllers;
+
+import android.os.Bundle;
+import android.text.InputType;
+
+import androidx.appcompat.app.AppCompatActivity;
+import androidx.preference.EditTextPreference;
+import androidx.preference.PreferenceFragmentCompat;
+
+import io.gejsi.pufferfish.R;
+
+public class SettingsActivity extends AppCompatActivity {
+  @Override
+  protected void onCreate(Bundle savedInstanceState) {
+    super.onCreate(savedInstanceState);
+    setContentView(R.layout.settings_activity);
+    if (savedInstanceState == null) {
+      getSupportFragmentManager().beginTransaction().replace(R.id.settings, new SettingsFragment()).commit();
+    }
+  }
+
+  public static class SettingsFragment extends PreferenceFragmentCompat {
+    @Override
+    public void onCreatePreferences(Bundle savedInstanceState, String rootKey) {
+      setPreferencesFromResource(R.xml.root_preferences, rootKey);
+
+      EditTextPreference signaturePref = findPreference("signature");
+      if (signaturePref != null)
+        signaturePref.setOnBindEditTextListener(editText -> editText.setInputType(InputType.TYPE_CLASS_NUMBER));
+    }
+  }
+}
