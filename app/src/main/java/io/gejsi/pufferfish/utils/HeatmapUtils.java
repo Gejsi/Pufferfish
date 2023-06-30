3b9ffda6b5b9c0945d69d4f52a6258bfeab72cf6
Gejsi <vgejsi@gmail.com>
2023-06-30 10:15:24 +0200

    Small ui changes

--- app/src/main/java/io/gejsi/pufferfish/utils/HeatmapUtils.java
+++ app/src/main/java/io/gejsi/pufferfish/utils/HeatmapUtils.java
@@ -1,16 +1,10 @@
 package io.gejsi.pufferfish.utils;
 
 import android.app.Activity;
-import android.graphics.Color;
-import android.widget.TextView;
 import android.widget.Toast;
 
 import androidx.annotation.NonNull;
 
-import com.github.mikephil.charting.charts.BarChart;
-import com.github.mikephil.charting.data.BarData;
-import com.github.mikephil.charting.data.BarDataSet;
-import com.github.mikephil.charting.data.BarEntry;
 import com.google.firebase.auth.FirebaseAuth;
 import com.google.firebase.auth.FirebaseUser;
 import com.google.firebase.database.DataSnapshot;
@@ -31,7 +25,6 @@ import java.io.FileWriter;
 import java.io.IOException;
 import java.io.InputStreamReader;
 import java.text.SimpleDateFormat;
-import java.util.ArrayList;
 import java.util.Arrays;
 import java.util.Collection;
 import java.util.Date;
@@ -263,71 +256,5 @@ public class HeatmapUtils {
     heatmapsRef.addValueEventListener(valueEventListener);
 
     return heatmapFuture;
-  }
-
-  public static void drawChart(Activity activity, BarChart intensityChart, Measurement.Type measurementType) {
-    FirebaseUser currentUser = FirebaseAuth.getInstance().getCurrentUser();
-    assert currentUser != null;
-
-    FirebaseDatabase database = FirebaseDatabase.getInstance(activity.getString(R.string.db));
-    DatabaseReference heatmapsRef = database.getReference("heatmaps").child(currentUser.getUid());
-
-    CompletableFuture<DataSnapshot> dataSnapshotFuture = new CompletableFuture<>();
-    heatmapsRef.addValueEventListener(new ValueEventListener() {
-      @Override
-      public void onDataChange(@NonNull DataSnapshot dataSnapshot) {
-        dataSnapshotFuture.complete(dataSnapshot);
-      }
-
-      @Override
-      public void onCancelled(@NonNull DatabaseError databaseError) {
-        Toast.makeText(activity, "Error while retrieving the heatmap.", Toast.LENGTH_SHORT).show();
-      }
-    });
-
-
-    dataSnapshotFuture.thenAccept(dataSnapshot -> {
-      int goodNum = 0;
-      int averageNum = 0;
-      int badNum = 0;
-
-      for (DataSnapshot heatmapSnapshot : dataSnapshot.getChildren()) {
-        Heatmap heatmap = heatmapSnapshot.getValue(Heatmap.class);
-        if (heatmap != null && heatmap.getMeasurementType() == measurementType) {
-          for (Measurement measurement : heatmap.getMeasurements().values()) {
-            if (measurement.getIntensity() == Measurement.Intensity.Good)
-              goodNum++;
-            else if (measurement.getIntensity() == Measurement.Intensity.Average)
-              averageNum++;
-            else if (measurement.getIntensity() == Measurement.Intensity.Bad)
-              badNum++;
-          }
-        }
-      }
-
-      float totalMeasurements = goodNum + averageNum + badNum;
-      float goodPercentage = (goodNum / totalMeasurements) * 100;
-      float averagePercentage = (averageNum / totalMeasurements) * 100;
-      float badPercentage = (badNum / totalMeasurements) * 100;
-
-      List<BarEntry> entries = new ArrayList<>();
-      if (totalMeasurements != 0) {
-        entries.add(new BarEntry(0f, goodPercentage));
-        entries.add(new BarEntry(1f, averagePercentage));
-        entries.add(new BarEntry(2f, badPercentage));
-
-        BarDataSet dataSet = new BarDataSet(entries, "Intensity Distribution");
-        dataSet.setColors(Color.GREEN, Color.YELLOW, Color.RED);
-        BarData barData = new BarData(dataSet);
-        barData.setBarWidth(0.5f);
-        intensityChart.setData(barData);
-        intensityChart.invalidate();
-
-        ((TextView) activity.findViewById(R.id.goodMeasurementsText)).setText("Number of good measurements: " + goodNum);
-        ((TextView) activity.findViewById(R.id.averageMeasurementsText)).setText("Number of average measurements: " + averageNum);
-        ((TextView) activity.findViewById(R.id.badMeasurementsText)).setText("Number of bad/poor measurements: " + badNum);
-        ((TextView) activity.findViewById(R.id.totalMeasurementsText)).setText("Total number of measurements: " + (int) totalMeasurements);
-      }
-    });
   }
 }
